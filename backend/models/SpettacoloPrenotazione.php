1a53ff903c4b9e9a837447b9cc9bac37b449651c
Mattia Web Designer <mattiawebdesigner@gmail.com>
2024-09-18 11:01:32 +0200

    Visualizzazione prenotazioni
    Aggiunta visualizzazione per le prenotazioni già effettuate (elenco dei nominativi con conteggio dei posti prenotati)

--- backend/models/SpettacoloPrenotazione.php
+++ backend/models/SpettacoloPrenotazione.php
@@ -3,6 +3,7 @@
 namespace backend\models;
 
 use Yii;
+use backend\components\sistema_prenotazione_biglietti\Postazioni;
 
 /**
  * This is the model class for table "{{%prenotazioni}}".
@@ -65,7 +66,7 @@ class SpettacoloPrenotazione extends \yii\db\ActiveRecord
     public static function totali($prenotazioni, $piantina){
         $nOfSeatBooked = 0;
         foreach ($prenotazioni as $prenotazione){            
-            $nOfSeatBooked += Postazioni::nOfSeatBooked($prenotazione->prenotazione, $prenotazione->abbonamento);
+            $nOfSeatBooked += Postazioni::nOfSeatBooked($prenotazione->prenotazione, /*$prenotazione->abbonamento*/"");
         }
         //------------------------------
         $nOfSeatState   = [];
@@ -78,12 +79,12 @@ class SpettacoloPrenotazione extends \yii\db\ActiveRecord
                 $nOfSeatState[$prenotazione->email]['tot'] = $res['tot'];
             }
             
-            if(!is_null($prenotazione->abbonamento)){
+            /*if(!is_null($prenotazione->abbonamento)){
                 $subscriber = Postazioni::nOfSeatState($piantina, $prenotazione->abbonamento);
                 $nOfSeatState[$prenotazione->email]['subcribers']['nOfSeatPaid'] = $subscriber['nOfSeatPaid'];
                 $nOfSeatState[$prenotazione->email]['subcribers']['nOfSeatNotPaid'] = $subscriber['nOfSeatNotPaid'];
                 $nOfSeatState[$prenotazione->email]['subcribers']['tot'] = $subscriber['tot'];
-            }
+            }*/
         }
         
         return [
