c6cde6b05138be3f7ae7777a4ce5abe03b27446c
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-07-24 13:56:27 +0200

    Update _approved.php

--- backend/views/iloveteatro/iscrizioni/btns/_approved.php
+++ backend/views/iloveteatro/iscrizioni/btns/_approved.php
@@ -3,4 +3,4 @@ use yii\helpers\Html;
 ?>
 <?= Html::a('<i class="fas fa-sign-in-alt"></i> '.Yii::t('app', 'Accetta iscrizione'), 
         ['approved-troupe', 'id' => $model->id], 
-        ['class' => 'btn btn-success']) ?>
+        ['class' => 'btn btn-success', 'title' => Yii::t('app', 'Confermando l\'iscrizione verrà inviata un\'email automatica alla compagnia')]) ?>
