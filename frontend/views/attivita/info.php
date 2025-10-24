d0fb57429f3f5bea56da33dce73de7d23e0f9a74
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-10-24 22:55:09 +0200

    Update info.php

--- frontend/views/attivita/info.php
+++ frontend/views/attivita/info.php
@@ -15,67 +15,67 @@ $this->title = $model->nome;
     <h1><?= Html::encode($this->title) ?></h1>
     
     <div class="row">
-		<div class="col">
-			<div class="wrapper">
-				<div class="img" style="background-image: url(<?= "../../backend/web/".$model->foto ?: "" ?>)"></div>
-			</div>
-			
-			<div class="content">
-                            <div class="place"><i class="fas fa-map-pin"></i> <?= $model->luogo ?></div>
-                            
-                            <?php        
-                            echo $this->render('sections/_turns',[
-                                'evento'        => $model,
-                            ]);
-                            ?>
-                            
-			</div>
-				
-			<?php if( ($model->posti_disponibili-$posti_occupati) == 0 && $model->posti_disponibili!=null) : ?>
-                            <div class="alert alert-warning">
-                                <?= Yii::t('app', 'Non ci sono posti disponibili')?>
-                            </div>
-			<?php endif; ?>
-				
-			<hr style="border-style: outline;border-width: 3px;border-color: #f0f0f0;" />
-			
-			<div class="reservation">
-                            <?php if($model->prenotazione == "yes"): ?>
-				<h5><?= Yii::t('app', 'Prenota') ?></h5>
-					
-    				<?php $form = ActiveForm::begin(); ?>
-                                        <?= $form->field($prenotazioni, 'cognome')->textInput(['maxlength' => true]) ?>
-                                        <?= $form->field($prenotazioni, 'nome')->textInput(['maxlength' => true]) ?>
-    					<?= $form->field($prenotazioni, 'email')->textInput(['type'=>'email', 'maxlength' => true]) ?>
-    					<?= $form->field($prenotazioni, 'prenotazioni')->textInput(['type'=>'number', 'min' => 1, 'max' => ($model->posti_disponibili-$posti_occupati)])->label(Yii::t('app', 'Numero di partecipanti')) ?>
-					<?= $form->field($prenotazioni, 'turno')->hiddenInput(['value'=>$turn])->label(false); ?>	
-                                        <div class="form-group">
-                                            <?= Html::submitButton(Yii::t('app', 'Prenota'), ['class' => 'btn btn-success']) ?>
-                                        </div>
-    				<?php ActiveForm::end(); ?>
-					
-                                <h5><?= Yii::t('app', 'Cerca una prenotazione') ?></h5>
-    				<?php $form = ActiveForm::begin(); ?>
-    					<input type="hidden" name="action" value="search" />
-    					
-    					<?= $form->field($prenotazioni, 'email')->textInput(['type'=>'email', 'maxlength' => true]) ?>
-					<?= $form->field($prenotazioni, 'turno')->hiddenInput(['value'=>$turn])->label(false); ?>
+        <div class="col">
+            <div class="wrapper">
+                    <div class="img" style="background-image: url(<?= "../../backend/web/".$model->foto ?: "" ?>)"></div>
+            </div>
 
-                                        <div class="form-group">
-                                            <?= Html::submitButton(Yii::t('app', 'Cerca'), ['class' => 'btn btn-success']) ?>
-                                        </div>
-    				<?php ActiveForm::end(); ?>
-					
-                            <?php endif;?>
-			</div>
-			
-			<div class="description">
-				<h5><?= Yii::t('app', "Note sull'evento") ?></h5>
-				
-				<?= $model->descrizione ?>
-			</div>
-		</div>
-	</div>
+            <div class="content">
+                <div class="place"><i class="fas fa-map-pin"></i> <?= $model->luogo ?></div>
+
+                <?php        
+                echo $this->render('sections/_turns',[
+                    'evento'        => $model,
+                ]);
+                ?>
+
+            </div>
+
+            <?php if( ($model->posti_disponibili-$posti_occupati) == 0 && $model->posti_disponibili!=null) : ?>
+                <div class="alert alert-warning">
+                    <?= Yii::t('app', 'Non ci sono posti disponibili')?>
+                </div>
+            <?php endif; ?>
+
+            <hr style="border-style: outline;border-width: 3px;border-color: #f0f0f0;" />
+
+            <div class="reservation">
+                <?php if($model->prenotazione == "yes"): ?>
+                    <h5><?= Yii::t('app', 'Prenota') ?></h5>
+
+                    <?php $form = ActiveForm::begin(); ?>
+                            <?= $form->field($prenotazioni, 'cognome')->textInput(['maxlength' => true]) ?>
+                            <?= $form->field($prenotazioni, 'nome')->textInput(['maxlength' => true]) ?>
+                            <?= $form->field($prenotazioni, 'email')->textInput(['type'=>'email', 'maxlength' => true]) ?>
+                            <?= $form->field($prenotazioni, 'prenotazioni')->textInput(['type'=>'number', 'min' => 1, 'max' => ($model->posti_disponibili-$posti_occupati)])->label(Yii::t('app', 'Numero di partecipanti')) ?>
+                            <?= $form->field($prenotazioni, 'turno')->hiddenInput(['value'=>$turn])->label(false); ?>	
+                            <div class="form-group">
+                                <?= Html::submitButton(Yii::t('app', 'Prenota'), ['class' => 'btn btn-success']) ?>
+                            </div>
+                    <?php ActiveForm::end(); ?>
+
+                    <h5><?= Yii::t('app', 'Cerca una prenotazione') ?></h5>
+                    <?php $form = ActiveForm::begin(); ?>
+                            <input type="hidden" name="action" value="search" />
+
+                            <?= $form->field($prenotazioni, 'email')->textInput(['type'=>'email', 'maxlength' => true]) ?>
+                            <?= $form->field($prenotazioni, 'turno')->hiddenInput(['value'=>$turn])->label(false); ?>
+
+                            <div class="form-group">
+                                <?= Html::submitButton(Yii::t('app', 'Cerca'), ['class' => 'btn btn-success']) ?>
+                            </div>
+                    <?php ActiveForm::end(); ?>
+
+                <?php endif;?>
+            </div>
+
+            <div class="description">
+                    <h5><?= Yii::t('app', "Note sull'evento") ?></h5>
+
+                    <?= $model->descrizione ?>
+            </div>
+        </div>
+    </div>
 
 </div>
 
