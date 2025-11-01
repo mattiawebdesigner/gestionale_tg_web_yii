847b1c39781b87fe715c7c61414462cba6d949d5
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-11-02 00:37:37 +0100

    Update create form

--- backend/views/attivita/_form.php
+++ backend/views/attivita/_form.php
@@ -16,37 +16,47 @@ use dosamigos\tinymce\TinyMce;
 
     <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
 
-	<div>
-		<div class="img-box">
-            <?= $form->field($model, 'foto')->textInput(['maxlength' => true, 'class' => 'append-url form-control', 'readonly' => 'readonly']) ?>
-            <div class="clear-img"><i class="fas fa-times-circle"></i></div>
-        </div>
-        
-        <div class="img img-show-media">
-        	<?= Html::img($model->foto, [
-        	    'style' => 'width: 250px;'
-        	])?>
-        </div>
-        
-        <div class="btn btn-info open-iframe"><?= Yii::t('app', 'Galleria immagini') ?></div>
-	</div>
-    
-<?= $form->field($model, 'descrizione')->widget(TinyMce::className(), [
-    'options' => ['rows' => 20],
-    'language' => 'it',
-    'clientOptions' => [
-        'plugins' => [
-            "advlist autolink lists link charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
-        ],
-        'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-    ]
-]);?>
+    <div>
+            <div class="img-box">
+        <?= $form->field($model, 'foto')->textInput(['maxlength' => true, 'class' => 'append-url form-control', 'readonly' => 'readonly']) ?>
+        <div class="clear-img"><i class="fas fa-times-circle"></i></div>
+    </div>
+
+    <div class="img img-show-media">
+            <?= Html::img($model->foto, [
+                'style' => 'width: 250px;'
+            ])?>
+    </div>
+
+    <div class="btn btn-info open-iframe"><?= Yii::t('app', 'Galleria immagini') ?></div>
+    </div>
+
+    <?= $form->field($model, 'descrizione')->widget(TinyMce::className(), [
+        'options' => ['rows' => 20],
+        'language' => 'it',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]);?>
 
     <?= $form->field($model, 'luogo')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'data_attivita')->textInput(['type' => 'datetime-local', 'value' => str_replace(" ", "T", $model->data_attivita)]) ?>
+    <?= $form->field($model, 'data_attivita')
+                ->textInput([
+                    'type' => 'datetime-local', 
+                    'value' => str_replace(" ", "T", $model->data_attivita)
+                ]) ?>
+    
+    <div data-generate>
+        <div data-add><i class="fa fa-plus"></i></div>
+        
+        <div data-append></div>
+    </div>
 	
     <?= Yii::t('app', 'Evento a pagamento?') ?>
 
@@ -101,12 +111,21 @@ $this->registerCss('
 
 $this->registerJsFile("@web/js/media.js", ['depends' => [yii\web\JqueryAsset::className()]]);
 $this->registerJsFile("@web/js/showItem.js", ['depends' => [yii\web\JqueryAsset::className()]]);
+$this->registerJsFile("@web/js/utility/generateElement.js", ['depends' => [yii\web\JqueryAsset::className()]]);
 $this->registerJs("jQuery('#iframe').media({
     open        : '.open-iframe',
     attachment  : 'input.append-url' 
 });
 jQuery('#showItem').showItem({
     clear : false
+});
+jQuery('[data-generate]').generateElement({
+    'element' : '<div class=\"form-group field-attivita-data_attivita\">' +
+                    '<label class=\"control-label\" for=\"attivita-data_attivita\">Data Attivita</label>' +
+                    '<input type=\"datetime-local\" id=\"attivita-data_attivita\" class=\"form-control\" name=\"Attivita[data_attivita][]\" value=\"\">' +
+                    
+                    '<div class=\"help-block\"></div>'+
+                '</div>'
 });
 ", View::POS_END);
 ?>
