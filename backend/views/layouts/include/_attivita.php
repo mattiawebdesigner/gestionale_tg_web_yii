170fc238cd4c150eee49943a9001c72cbce4f070
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-11-06 09:38:40 +0100

    Add menu item

--- backend/views/layouts/include/_attivita.php
+++ backend/views/layouts/include/_attivita.php
@@ -1,5 +1,6 @@
 <?php
 
 return ['label' => 'Attivita', 'url' => ['/attivita/index'], 'items' => [
+    ['label' => Yii::t('app', 'Tutte le attività'), 'url' => ['/attivita/index']],
     ['label' => Yii::t('app', 'Elenco prenotazioni'), 'url' => ['/attivita/reservations']],
 ]];
