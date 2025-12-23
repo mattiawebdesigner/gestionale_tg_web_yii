5345cda49fe0d934ef47fe31463a6024a16e19a1
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-12-23 19:12:25 +0100

    Added category page

---
+++ backend/views/tg-site/categorie.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Url;
+
+$this->title = Yii::t('app', 'Categorie per gli articoli');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tgsite-index">
+    <div class="body-content">
+        <h1><?= $this->title ?></h1>
+    </div>
+    
+    
+</div>
+
+<?php
+$this->registerCssFile("@web/css/tg-site/style.css");
