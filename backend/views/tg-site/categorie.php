581db062ed20a24b1caf2ea06e4df331a57d705c
Mattia Web Designer <mattiawebdesigner@gmail.com>
2025-12-24 13:18:08 +0100

    Create categorie's page

---
+++ backend/views/tg-site/categorie.php
@@ -0,0 +1,14 @@
+<?php
+$this->title = Yii::t('app', 'Categorie per gli articoli');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tgsite-index">
+    <div class="body-content">
+        <h1><?= $this->title ?></h1>
+    </div>
+    
+    
+</div>
+
+<?php
+$this->registerCssFile("@web/css/tg-site/style.css");
