24ae26b1a5d79a5d858a76b531c6cca42424035e
Mattia Web Designer <mattiawebdesigner@gmail.com>
2026-02-22 10:50:41 +0100

    Modified menu segreteria
    The menu is now sho in horizontal version.
    This style is used to prevent that the menu displayed outside viewport

--- backend/views/layouts/include/_segreteria.php
+++ backend/views/layouts/include/_segreteria.php
@@ -1,40 +1,42 @@
 <?php
 return ['label' => 'Segreteria', 'url' => '#', 'items' => [
             //Soci
-            ['label' =>  Yii::t('app', 'I Soci')],
-            ['label' =>  Yii::t('app', 'Anno Sociale'), 'url' => ['/anno-sociale/index']],
-            ['label' =>  Yii::t('app', 'Soci suddivisi per anno'), 'url' => ['/soci/index']],
-            ['label' =>  Yii::t('app', 'Storico dei soci'), 'url' => ['/soci/all']],
-            ['label' =>  Yii::t('app', 'Soci in corso di validità'), 'url' => ['/soci/print-show']],
-            '<hr />',
-    
-            //Consiglio direttivo
-            ['label' => Yii::t('app', 'Consiglio Direttivo')],
-            ['label' => Yii::t('app', 'Componenti consiglio direttivo'), 'url' => ['/direttivo/index']],
-            '<hr />',            
-    
-            //Albo d'oro
-            ['label' => 'Albo D\'Oro'],
-            ['label' => Yii::t('app', 'Nuovo Componente'), 'url' => ['/nominativo/create']],
-            ['label' => Yii::t('app', 'I Componenti'), 'url' => ['/nominativo/index']],
-            '<hr />',
+            "<div class='menu-item'>",
+                ['label' =>  Yii::t('app', 'I Soci')],
+                ['label' =>  Yii::t('app', 'Anno Sociale'), 'url' => ['/anno-sociale/index']],
+                ['label' =>  Yii::t('app', 'Soci suddivisi per anno'), 'url' => ['/soci/index']],
+                ['label' =>  Yii::t('app', 'Storico dei soci'), 'url' => ['/soci/all']],
+                ['label' =>  Yii::t('app', 'Soci in corso di validità'), 'url' => ['/soci/print-show']],
+                '<hr />',
+
+                //Consiglio direttivo
+                ['label' => Yii::t('app', 'Consiglio Direttivo')],
+                ['label' => Yii::t('app', 'Componenti consiglio direttivo'), 'url' => ['/direttivo/index']],
+                '<hr />',            
+
+                //Albo d'oro
+                ['label' => 'Albo D\'Oro'],
+                ['label' => Yii::t('app', 'Nuovo Componente'), 'url' => ['/nominativo/create']],
+                ['label' => Yii::t('app', 'I Componenti'), 'url' => ['/nominativo/index']],
+            "</div>",
 
             //Rendiconti
-            ['label' => Yii::t('app', 'Rendicontazioni')],
-            ['label' => Yii::t('app', 'Le rendicontazioni'), 'url' => ['/rendiconto/index']],
-            ['label' => Yii::t('app', 'Nuova rendicontazione'), 'url' => ['/rendiconto/create']],
-            '<hr />',
-    
-            //Verbali
-            ['label' => Yii::t('app', 'Gestione dei verbali')],
-            ['label' => Yii::t('app', 'Le Convocazioni'), 'url' => ['/convocazioni/index']],
-            ['label' => Yii::t('app', 'I verbali'), 'url' => ['/verbali/index']],
-            '<hr />',
-            
-            //Documentazione
-            ['label' => Yii::t('app', 'Documenti')],
-            ['label' => Yii::t('app', 'Documentazione'), 'url' => ['/documentazione/index']],
-            '<hr />',
+            "<div class='menu-item'>",
+                ['label' => Yii::t('app', 'Rendicontazioni')],
+                ['label' => Yii::t('app', 'Le rendicontazioni'), 'url' => ['/rendiconto/index']],
+                ['label' => Yii::t('app', 'Nuova rendicontazione'), 'url' => ['/rendiconto/create']],
+                '<hr />',
+
+                //Verbali
+                ['label' => Yii::t('app', 'Gestione dei verbali')],
+                ['label' => Yii::t('app', 'Le Convocazioni'), 'url' => ['/convocazioni/index']],
+                ['label' => Yii::t('app', 'I verbali'), 'url' => ['/verbali/index']],
+                '<hr />',
+
+                //Documentazione
+                ['label' => Yii::t('app', 'Documenti')],
+                ['label' => Yii::t('app', 'Documentazione'), 'url' => ['/documentazione/index']],
+            "</div>",
 
             //Email
             /*['label' => Yii::t('app', 'Gestione mail')],
@@ -44,7 +46,9 @@ return ['label' => 'Segreteria', 'url' => '#', 'items' => [
             '<hr />',*/
 
             //Votazioni
-            ['label' => Yii::t('app', 'Votazioni')],
-            //['label' => Yii::t('app', 'Genera elenco soci aventi diritto al voto'), 'url' => ['/soci/elenco-votazioni']],
-            ['label' => Yii::t('app', 'Gestione delle votazioni'), 'url' => ['/votazione/gestione-votazioni']],
+            "<div class='menu-item'>",
+                ['label' => Yii::t('app', 'Votazioni')],
+                //['label' => Yii::t('app', 'Genera elenco soci aventi diritto al voto'), 'url' => ['/soci/elenco-votazioni']],
+                ['label' => Yii::t('app', 'Gestione delle votazioni'), 'url' => ['/votazione/gestione-votazioni']],
+            "</div>",
         ]];
