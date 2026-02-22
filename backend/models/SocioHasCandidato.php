4b8f4b7a806f5f8ddcedaa9b06e67f201e7acdea
Mattia Web Designer <mattiawebdesigner@gmail.com>
2026-02-22 18:28:30 +0100

    Added new model: SocioHasCandidato
    This model is used to join vote and candidates

---
+++ backend/models/SocioHasCandidato.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%socio_has_candidato}}".
+ *
+ * @property int $socio_id
+ * @property string $time_candidatura
+ * @property int $votazione_id
+ */
+class SocioHasCandidato extends \yii\db\ActiveRecord
+{
+    
+    /**
+     * File da caricare
+     */
+    public $file;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%socio_has_candidato}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['socio_id', 'votazione_id'], 'required'],
+            [['socio_id', 'votazione_id'], 'int'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'socio_id' => Yii::t('app', 'ID del socio'),
+            'time_candidatura' => Yii::t('app', 'Data e orario di candidatura'),
+            'votazione_id' => Yii::t('app', 'Data ID della votazione'),
+        ];
+    }
+
+    /**
+     * Gets query for [[socio]].
+     *
+     * @return \yii\db\ActiveQuery|SociQuery
+     */
+    public function getSocio()
+    {
+        return $this->hasMany(Soci::className(), ['socio_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Votazione]].
+     *
+     * @return \yii\db\ActiveQuery|SocioAnnoSocialeQuery
+     */
+    public function getVotazione()
+    {
+        return $this->hasMany(Votazione::className(), ['socio' => 'id']);
+    }
+    
+    public function getFirma()
+    {
+        return $this->hasMany(Firma::className(), ['votazione_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return SociQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new SocioHasCandidatoQuery(get_called_class());
+    }
+}
