da302d091e334f37e6013069c9cfa3eaeb01d2e0
Mattia Web Designer <mattiawebdesigner@gmail.com>
2026-03-07 13:47:57 +0100

    updates

---
+++ api/config/main-local.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '7H6QqdMREs5pV4fhXRll4ByGiML8c2V1', 
+
+        ],
+    ],
+];
